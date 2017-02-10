3a8e2c49d5c4978878e7b5646d8abd2ff6ec04a3
Ryan Abel <ryan.abel@atomicobject.com>
2017-02-10 09:38:49 -0500

    Precompile assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: 'erinryan', password: 'th3abel3s'
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] 
 end
