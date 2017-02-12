b97698433d5a3ad480bf4e04c56c3f0204d3e7be
Ryan Abel <ryan.abel@atomicobject.com>
2017-02-11 19:29:04 -0500

    Fix date format

--- lib/charge_repository.rb
+++ lib/charge_repository.rb
@@ -1,7 +1,7 @@
 class ChargeRepository
   def self.parse_and_create_charges(charge_arr)
     charge_arr.each do |charge_row|
-      date = Date.strptime(charge_row[2], "%m/%d/%y")
+      date = Date.strptime(charge_row[2], "%m/%d/%Y")
       amount = charge_row[4].to_f * -1
       description = charge_row[3].strip
       charge = Charge.new(transaction_date: date, description: description, amount: amount)
