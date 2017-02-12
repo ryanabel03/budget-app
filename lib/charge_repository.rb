fe3452f41c2a86f9bd7766bf7478a41f27c48528
Ryan Abel <ryan.abel@atomicobject.com>
2017-02-12 15:35:00 -0500

    Fix

--- lib/charge_repository.rb
+++ lib/charge_repository.rb
@@ -4,8 +4,7 @@ class ChargeRepository
       date = Date.strptime(charge_row[2], "%m/%d/%Y")
       amount = charge_row[4].to_f * -1
       description = charge_row[3].strip
-      charge = Charge.new(transaction_date: date, description: description, amount: amount)
-      charge.save!
+      Charge.create!(transaction_date: date, description: description, amount: amount)
     end
   end
 
